7bfe4fae330c582b4c9f4a09ff04049f746ae836
cad435 <cad435.t11a@gmail.com>
2020-08-22 17:42:03 +0200

    Configured PID-Autotune

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -490,9 +490,14 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  //#define DEFAULT_Kp 22.2
+  //#define DEFAULT_Ki 1.08
+  //#define DEFAULT_Kd 114
+
+  //Self Measured
+  #define DEFAULT_Kp 25.93
+  #define DEFAULT_Ki 2.31
+  #define DEFAULT_Kd 72.75
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
