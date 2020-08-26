67ce14f6227fd6fee0c0b45c69eddce195c07362
cad435 <cad435.t11a@gmail.com>
2020-08-26 21:45:43 +0200

    tried to get the geared-E running...

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -752,14 +752,18 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 160, 400, 139 , 139} //Getunte config
+#define DEFAULT_AXIS_ESTEPS0_PER_UNIT_N 139 //used for Custom Command
+#define DEFAULT_AXIS_ESTEPS0_PER_UNIT_G 8340 //139*60 //used for Custom Command
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 160, 400, DEFAULT_AXIS_ESTEPS0_PER_UNIT_N , DEFAULT_AXIS_ESTEPS0_PER_UNIT_N} 
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 150, 150, 12.5, 40 }
+#define DEFAULT_MAX_E1_FEEDRATE_N 40 //used for Custom Command
+#define DEFAULT_MAX_E1_FEEDRATE_G 2 //used for Custom Command
+#define DEFAULT_MAX_FEEDRATE          { 150, 150, 12.5, DEFAULT_MAX_E1_FEEDRATE_N, DEFAULT_MAX_E1_FEEDRATE_N}
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -772,7 +776,9 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 500, 500, 10, 10000 }
+#define DEFAULT_MAX_E1_ACCELERATION_N 500
+#define DEFAULT_MAX_E1_ACCELERATION_G 10
+#define DEFAULT_MAX_ACCELERATION      { 500, 500, DEFAULT_MAX_E1_ACCELERATION_N, DEFAULT_MAX_E1_ACCELERATION_N}
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -787,9 +793,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          3000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)
